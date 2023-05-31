acd00889f161a67887744f23dc1c843fe4a3465f
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-05-31 22:35:34 +0100

    Maps

---
+++ 5_Arrays/src/App.java
@@ -0,0 +1,27 @@
+
+public class App {
+    public static void main(String[] args) {
+        int[] arrayOfNumbers = {1, 2, 3, 4, 5};
+        System.out.println(arrayOfNumbers[1]);
+        
+        for (int element: arrayOfNumbers) {
+            System.out.println(element);
+        }
+        
+        arrayOfNumbers[2] = 12;
+        System.out.println(arrayOfNumbers[2]);
+        
+        
+        Integer[] myIntArray = {1, 2, 3, 4, 5};
+
+        java.util.List<Integer> myList = java.util.Arrays.asList(myIntArray);
+        java.util.ArrayList<Integer> myArrayList = new java.util.ArrayList<Integer>(myList);
+
+        myArrayList.set(2, 10);
+        myArrayList.add(50);
+
+        for (int element: myArrayList) {
+            System.out.println(element);
+        }
+    }
+}
